b737ea895dcb0bfc1a158f50ab5d885587b3abb8
João Castro <jvinicius2002@gmail.com>
2023-10-15 22:53:18 -0300

    Console Log

--- src/components/RegistrarIngresso.js
+++ src/components/RegistrarIngresso.js
@@ -81,6 +81,11 @@ const RegistrarIngresso = () => {
 
   const gerarPDF = async () => {
     if (nome && contato) { 
+
+    console.log('Nome:', nome); // Adicione esta linha
+    console.log('Contato:', contato); // Adicione esta linha
+    console.log('Número:', numero); // Adicione esta linha
+    console.log('Lido:', lido);
       const novoIngresso = {
         nome,
         contato,
