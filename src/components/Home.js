a1f05349b9288b3b500667429762d78fabde20ad
João Castro <jvinicius2002@gmail.com>
2023-10-18 20:32:00 -0300

    Atualizações de CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Container = styled.div`
   flex-direction: column;
   //align-items: center;
   //justify-content: center;
-  //height: 100vh;
+  height: 100vh;
   background-color: #f7f7f7;
   @media (max-width: 768px) {
     align-items: center; 
