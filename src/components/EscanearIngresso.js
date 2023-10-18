796ce19584facc0d6e627d494c0ac32d4e6a9ac3
Eric Rodrigues de Oliveira <93745627+ericrodrigues00@users.noreply.github.com>
2023-10-17 22:01:46 -0300

    Update EscanearIngresso.js2

--- src/components/EscanearIngresso.js
+++ src/components/EscanearIngresso.js
@@ -12,15 +12,41 @@ const Container = styled.div`
   min-height: 100vh;
 `;
 
-// ... (rest of your styled components)
+const Title = styled.h1`
+  color: #6a1b9a;
+  margin-bottom: 20px;
+`;
+
+const ScannerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+`;
+
+const QRScanner = styled(QrScanner)`
+  max-width: 100%;
+`;
+
+const Popup = styled.div`
+  background-color: #6a1b9a;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  z-index: 1;
+`;
 
 const EscanearIngresso = () => {
   const [resultadoScan, setResultadoScan] = useState(null);
   const [popupVisible, setPopupVisible] = useState(false);
   const [redirect, setRedirect] = useState(null);
   const [lido, setLido] = useState(false);
-
-  // Adicione um estado para controlar a leitura do ingresso
   const [leituraAtiva, setLeituraAtiva] = useState(true);
 
   const handleError = (error) => {
@@ -35,17 +61,28 @@ const EscanearIngresso = () => {
   const verificarIngresso = async (result) => {
     if (leituraAtiva) {
       try {
-        // Resto do seu código de verificação do ingresso
+        const qrCodeData = result.text;
+        const numeroMatch = qrCodeData.match(/Numero: (\d+)/);
+        if (numeroMatch) {
+          const numero = numeroMatch[1];
+          const apiUrl = `https://api-eztickets.onrender.com/api/verificarIngresso?numero=${numero}`;
+          const response = await axios.get(apiUrl);
 
-        if (response.data.ingressoValido) {
-          setResultadoScan('Ingresso Válido');
-          setPopupVisible(true);
-          setLido(true);
-
-          // Pausar a leitura após a validação do ingresso
-          setLeituraAtiva(false);
+          if (response.data.ingressoValido) {
+            setResultadoScan('Ingresso Válido');
+            setPopupVisible(true);
+            setLido(true);
+            setLeituraAtiva(false); // Pausar a leitura após a validação do ingresso
+          } else {
+            if (response.data.message === "Ingresso já foi utilizado") {
+              setResultadoScan("Ingresso já foi utilizado");
+            } else {
+              setResultadoScan('Ingresso Inválido');
+            }
+            setPopupVisible(true);
+          }
         } else {
-          // Resto do seu código de tratamento para ingresso inválido
+          console.error('Número não encontrado no código QR');
         }
       } catch (error) {
         console.error('Erro ao verificar o ingresso:', error);
@@ -53,7 +90,6 @@ const EscanearIngresso = () => {
     }
   };
 
-  // Adicione um manipulador de evento para reiniciar a leitura
   const reiniciarLeitura = () => {
     setLeituraAtiva(true);
     setResultadoScan(null);
