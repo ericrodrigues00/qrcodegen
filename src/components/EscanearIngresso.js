34322784c6e1bb30838049cb29413eaf75174f05
MarcoSchwepps <142013967+MarcoSchwepps@users.noreply.github.com>
2023-10-19 15:19:00 -0300

    reatualização popup

--- src/components/EscanearIngresso.js
+++ src/components/EscanearIngresso.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import QrScanner from 'react-qr-scanner';
 import styled from 'styled-components';
 import axios from 'axios';
-import RegistrarIngresso from './RegistrarIngresso';
+import { FaCheck, FaSadCry, FaKissBeam } from 'react-icons/fa';
 
 const Container = styled.div`
   display: flex;
@@ -53,22 +53,62 @@ const QRScanner = styled(QrScanner)`
 `;
 
 const Popup = styled.div`
-  background-color: red;
+  background-color: white;
   color: #000;
-  padding: 10px 20px;
   border-radius: 5px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  z-index: 2;
+`;
+
+const PopupOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
   z-index: 1;
 `;
+
+const PopupContent = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin: 50px 90px 40px 90px;
+`;
+
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 70px;
+`;
+
+const IconType = ({ message }) => {
+  if (message === 'Ingresso Válido!') {
+    return (
+      <Icon>
+        <FaKissBeam size={64} color="#6a1b9a" />
+      </Icon>
+    );
+  } else {
+    return (
+      <Icon>
+        <FaSadCry size={64} color="red" />
+      </Icon>
+    );
+  }
+};
 
 const EscanearIngresso = () => {
   const [resultadoScan, setResultadoScan] = useState(null);
   const [popupVisible, setPopupVisible] = useState(false);
   const [redirect, setRedirect] = useState(null);
   const [lido, setLido] = useState(false);
+  const [codigoAnterior, setCodigoAnterior] = useState(null);
   const [bloquearLeitura, setBloquearLeitura] = useState(false);
 
   useEffect(() => {
@@ -84,12 +124,23 @@ const EscanearIngresso = () => {
     console.error('Erro ao escanear o QR Code:', error);
   };
 
+  const fecharPopup = () => {
+    setResultadoScan(null);
+    setPopupVisible(false);
+  };
+
   const verificarIngresso = async (result) => {
     if (bloquearLeitura) {
       return;
     }
 
-    setBloquearLeitura(true);
+    if (result.text === codigoAnterior) {
+      setBloquearLeitura(true);
+      return;
+    }
+
+    setBloquearLeitura(false);
+    setCodigoAnterior(result.text);
 
     try {
       const qrCodeData = result.text;
@@ -99,14 +150,15 @@ const EscanearIngresso = () => {
         const apiUrl = `https://api-eztickets.onrender.com/api/verificarIngresso?numero=${numero}`;
         const response = await axios.get(apiUrl);
         if (response.data.ingressoValido) {
-          setResultadoScan('Ingresso Válido');
+          setResultadoScan('Ingresso Válido!');
           setPopupVisible(true);
           setLido(true);
         } else {
-          if (response.data.message === "Ingresso já foi utilizado") {
-            setResultadoScan("Ingresso já foi utilizado");
-          } else {
-            setResultadoScan('Ingresso Inválido');
+          if (response.data.message === "Ingresso já foi utilizado"){
+            setResultadoScan("Ingresso já foi utilizado!")
+          }
+          else {
+            setResultadoScan('Ingresso Inválido!');
           }
           setPopupVisible(true);
         }
@@ -118,31 +170,36 @@ const EscanearIngresso = () => {
     }
   };
 
-  const fecharPopup = () => {
-    setResultadoScan(null);
-    setPopupVisible(false);
-  };
+  
 
   return (
     <Container>
       <TitleContainer>
-        <Title>
-          <StyledLink to="/">PARMEJÓ 2023</StyledLink>
-        </Title>
+      <Title>
+        <StyledLink to="/">PARMEJÓ 2023</StyledLink>
+      </Title>
       </TitleContainer>
       <ScannerContainer>
         <QRScanner
           onError={handleError}
           constraints={{
-            video: { facingMode: 'environment' },
+            video: { facingMode: 'environment' }, 
           }}
           onScan={(result) => {
             if (result) {
-              verificarIngresso(result);
+              verificarIngresso(result); 
             }
           }}
         />
-        {resultadoScan && <Popup onClick={fecharPopup}>{resultadoScan}</Popup>}
+        {resultadoScan && (
+          <div>
+            <PopupOverlay visible={popupVisible} onClick={fecharPopup} />
+            <Popup onClick={fecharPopup}>
+              <IconType message={resultadoScan} />
+              <PopupContent>{resultadoScan}</PopupContent>
+            </Popup>
+          </div>
+        )}
       </ScannerContainer>
     </Container>
   );
