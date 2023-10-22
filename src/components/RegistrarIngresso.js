9cc7508114094d4b25d695005c3cd006c56d32d7
MarcoSchwepps <142013967+MarcoSchwepps@users.noreply.github.com>
2023-10-22 14:23:33 -0300

    ALteração mauro css registrar

--- src/components/RegistrarIngresso.js
+++ src/components/RegistrarIngresso.js
@@ -7,43 +7,59 @@ import QRCode from "qrcode";
 // conteiner roxo
 
 const Container = styled.div`
-  padding: 20px;
-  background-color: #a391d6;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: 100vh;`;
-
-const Title = styled.h1`
-  text-transform: uppercase;
-  text-align: left;
-  font-family: "Outfit";
+  flex-direction: column;
+  min-height: 100vh;
   @media (max-width: 768px) {
-    font-size: 30px;
-    text-align: CENTER;
+    align-items: center; 
+    height: 100%;
   }
 `;
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; /* Alinhar o texto à esquerda */
+  margin: 20px 20px 60px 60px;
+  text-transform: uppercase;
+  @media (max-width: 768px) {
+    align-items: center; 
+    margin: 0px;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 46px;
+  font-family: "Outfit";
+  font-weight: bold;
+
+`;
+
 //conteiner branco com as infos
 const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
+  gap: 30px;
   background-color: white;
   border-radius: 6px;
   font-family: "Outfit";
-  box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
-  padding: 60px 50px;
+  padding: 0px 30px;
   max-width: 1000px;
-  width: 50%;
+  width: 100%;
   height: 50%;
   margin: 0 auto;
   justify-content:center;
+  font-size: 26px;
+  @media (max-width: 768px) {
+    align-items: center; 
+    padding: 20px 30px;
+    gap: 25px;
+  }
 `;
 
 
 const InputLabel = styled.label`
-  font-weight: bold;
   color: #6a1b9a;
   font-family: "Outfit";
   text-transform: uppercase;
@@ -54,8 +70,8 @@ const Input = styled.input`
   border: 0;
   border-radius: 0px;
   border-bottom: 1px solid #666;
-  width: 45%;
-  font-weight: bold;
+  width: 60%;
+  font-size: 20px;
   font-family: "Outfit";
   @media (max-width: 768px) {
     width: 70%;
@@ -64,17 +80,48 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-  background-color: #6a1b9a;
-  color: white;
-  padding: 10px 20px;
+  background-color: white;
+  color: black;
   border: none;
-  border-radius: 3px;
+  border-radius: 5px;
   cursor: pointer;
+  padding: 15px 40px 10px 40px;
+  font-size: 20px;
+  text-transform: uppercase;
   transition: background-color 0.3s ease;
   font-family: "Outfit";
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  position: relative;
+
   /*&:hover {
     background-color: #a391d6;
   }*/
+
+  &:after {
+    content: "";
+    height: 4px;
+    background-color: #6a1b9a;
+    width: 10%; 
+    position: absolute;
+    top: 0; 
+    left: 0; 
+    border-radius: 5px;
+    transition: width 0.5s ease;
+  }
+
+  &:hover:after {
+    width: 100%;
+  }
+
+  @media (max-width: 768px) { 
+    font-size: 16px; 
+    &:last-child {
+      margin-bottom: 20px;
+    }
+  }
 `;
 
 const QRCodeContainer = styled.div`
@@ -129,12 +176,22 @@ const RegistrarIngresso = () => {
 
         // Defina estilos de fonte, cor de fundo e tamanho de fonte
         doc.setFont("helvetica", "bold");
-        doc.setFontSize(18); // Tamanho da fonte aumentado para 18
+        doc.setFontSize(46); // Tamanho da fonte aumentado para 18
         doc.setTextColor(255, 255, 255);
-        doc.setFillColor(106, 27, 154);
+        doc.setFillColor(154, 95, 192);
         doc.rect(0, 0, 210, 297, "F");
-        doc.text("Parmejó 2023", pageWidth / 2, 30, null, null, "center");
-        const qrCodeSize = 70;
+        const textX = pageWidth / 2; // Centraliza horizontalmente
+        doc.text("PARMEJÓ 2023", textX, 40, null, null, "center");
+        doc.setFontSize(32); 
+        doc.text(
+          `ID: ${response.data.numero}`,
+          textX,
+          60,
+          null,
+          null,
+          "center"
+        );
+        const qrCodeSize = 75;
         const qrCodeConfiguration = {
           margin: 0,
           width: qrCodeSize,
@@ -143,7 +200,6 @@ const RegistrarIngresso = () => {
         // Gere o QR Code diretamente no PDF
         const qrCodeData = `Nome: ${response.data.nome}, Contato: ${response.data.contato}, Numero: ${response.data.numero}`;
 
-
         const canvas = document.createElement("canvas");
 
         QRCode.toCanvas(canvas, qrCodeData, qrCodeConfiguration);
@@ -151,20 +207,22 @@ const RegistrarIngresso = () => {
 
         // Calcula a posição central para o QR Code
         const qrCodeX = contentX + (contentWidth - qrCodeSize) / 2;
-        const qrCodeY = 50; // Define a posição vertical
+        const qrCodeY = 70; // Define a posição vertical
 
         // Exibir nome e contato no centro
-        const textX = pageWidth / 2; // Centraliza horizontalmente
-        const textY = qrCodeY + qrCodeSize + 10;
-        doc.text(`${response.data.nome}`, textX, textY, null, null, "center");
+        
+        const textY = qrCodeY + qrCodeSize + 25;
+        doc.setFontSize(28);
+        doc.text(`NOME: ${response.data.nome}`, textX, textY, null, null, "center");
         doc.text(
-          `${response.data.contato}`,
+          `EMAIL: ${response.data.contato}`,
           textX,
-          textY + 10,
+          textY + 15,
           null,
           null,
           "center"
         );
+
 
         // Defina a posição e o tamanho do QR Code
         doc.addImage(imgData, "PNG", qrCodeX, qrCodeY, qrCodeSize, qrCodeSize);
@@ -191,19 +249,22 @@ const RegistrarIngresso = () => {
 
   return (
     <Container>
+      <TitleContainer>
+        <Title>Registrar Ingresso</Title>
+      </TitleContainer>
       <FormContainer>
-      <Title>Registrar Ingresso</Title>
         <InputLabel>Nome:</InputLabel>
         <Input
           type="text"
           value={nome}
           onChange={(e) => setNome(e.target.value)}
         />
-        <InputLabel>E-mail:</InputLabel>
+        <InputLabel>Email:</InputLabel>
         <Input
           type="text"
           class="botãonome"
           value={contato}
+          placeholder="email@email.com"
           onChange={(e) => setContato(e.target.value)}
         />
         <Button onClick={gerarPDF}>SALVAR QRCODE</Button>
